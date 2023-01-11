600881e249ef40885d48f9e4b4f409ad352e173a
NicksFed <nick.guzy76@gmail.com>
2023-01-10 16:34:32 -0800

    WIP Will change background color on mouse motion
    Co-authored-by: James Provance <jb224x7@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
+// window.onload = function() {
+//   let h1 = document.querySelector("h1");
+//   h1.onmouseover = function() {
+//     window.alert("I am a heading element.");
+//   };
+
+//   let p = document.querySelector("p");
+
+//   p.onmouseover = function() {
+//     document.querySelector("p>em").innerText = "Don't be surprised";
+//   };
+
+//   let img = document.querySelector("img");
+
+//   img.onmouseover = function() {
+//     img.style.height = "700px";
+//   };
+// }
+
 window.onload = function() {
+  // let body = document.querySelector("body");
   let h1 = document.querySelector("h1");
-  h1.onmouseover = function() {
-    window.alert("I am a heading element.");
+  h1.onmouseover = function bob() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+    let number = console.log(getRandomInt(256));
   };
-
-  let p = document.querySelector("p");
-
-  p.onmouseover = function() {
-    document.querySelector("p>em").innerText = "Don't be surprised";
+  h1.onmouseover = function bb() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+    let number = console.log(getRandomInt(256));
   };
-
-  let img = document.querySelector("img");
-
-  img.onmouseover = function() {
-    img.style.height = "700px";
+  h1.onmouseover = function b() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+    let number = console.log(getRandomInt(256));
   };
 }
